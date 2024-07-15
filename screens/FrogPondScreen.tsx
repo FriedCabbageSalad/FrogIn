7188e727ab6b76b85635266be987ac373ad62331
Kieran <e1156748@u.nus.edu>
2024-07-15 16:21:15 +0800

    Added the random motivational message and associated UI Added all frogs to the FrogPond Screen

--- screens/FrogPondScreen.tsx
+++ screens/FrogPondScreen.tsx
@@ -8,11 +8,22 @@ import {
     TouchableOpacity,
     Image,
 } from 'react-native';
-import { frogDirectories, dimensions} from './../screens/Scripts.tsx'
+import { frogDirectories, dimensions, defaultFrogIndex, GachaNumberGenerator} from './../screens/Scripts.tsx'
 import { ud } from './../screens/HomeScreen.tsx'
 import auth from '@react-native-firebase/auth';
 
+const  motivationalMessages = [
+    "Believe in yourself and you will be unstoppable.",
+    "Challenges are what make life interesting. Overcoming them is what makes life meaningful.",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    "Don't watch the clock, do what it does: keep going.",
+    "Difficult roads often lead to beautiful destinations.",
+    "You didn't come this far to only come this far.",
+    "Every day is a new beginning. Take a deep breath, smile, and start again."
+]
+
 function FrogPondScreen({navigation}: {navigation: any}) {
+    const index = GachaNumberGenerator() % 8
     return (
         <ImageBackground source={require('./../assets/frog_pond_background.png')} resizeMode='cover' style={styles.imageSizing}>
             <View style={{position: 'absolute', right: 0, borderRadius: 10, margin: 7, padding: 3, backgroundColor: '#516D67'}}>  
@@ -24,9 +35,49 @@ function FrogPondScreen({navigation}: {navigation: any}) {
                     <Image source={require('./../assets/log_out.png')} resizeMode='contain' style={{width: 20, height: 20}}/>
                 </TouchableOpacity>
             </View>
-            {/* Frog */}
+            
+            <View style={styles.messageBox}>
+                <Text style={{fontSize: 13, fontWeight: 'bold'}}>Motivational Message</Text>
+                <Text>{motivationalMessages[index]}</Text>
+            </View>
+            {/* Frog Displays */}
+            {/* If Else Statement for frogs, basically if he has the frog, then whatever is below, else empty view */}
+            {/* arr[index] != 0 ? (the view) : <View></View> */}
+            {/* Default */}
             <View style={{position: 'absolute', top: dimensions()._height * 0.4, left: dimensions()._width * 0.45}}>
-                <Image source={frogDirectories[3].image}/>
+                <Image source={frogDirectories[defaultFrogIndex].image}/>
+            </View>
+            {/* Blue */}
+            <View style={{position: 'absolute', top: dimensions()._height * 0.45, left: dimensions()._width * 0.6}}>
+                <Image source={frogDirectories[defaultFrogIndex + 1].image}/>
+            </View>
+            {/* Ocean */}
+            <View style={{position: 'absolute', top: dimensions()._height * 0.35, left: dimensions()._width * 0.2}}>
+                <Image source={frogDirectories[defaultFrogIndex + 2].image}/>
+            </View>
+            {/* Gray */}
+            <View style={{position: 'absolute', top: dimensions()._height * 0.3, left: dimensions()._width * 0.4}}>
+                <Image source={frogDirectories[defaultFrogIndex + 3].image}/>
+            </View>
+            {/* Purple */}
+            <View style={{position: 'absolute', top: dimensions()._height * 0.35, left: dimensions()._width * 0.58}}>
+                <Image source={frogDirectories[defaultFrogIndex + 4].image}/>
+            </View>
+            {/* Red */}
+            <View style={{position: 'absolute', top: dimensions()._height * 0.42, left: dimensions()._width * 0.1}}>
+                <Image source={frogDirectories[defaultFrogIndex + 5].image}/>
+            </View>
+            {/* White */}
+            <View style={{position: 'absolute', top: dimensions()._height * 0.43, left: dimensions()._width * 0.3}}>
+                <Image source={frogDirectories[defaultFrogIndex + 6].image}/>
+            </View>
+            {/* Dark Gray */}
+            <View style={{position: 'absolute', top: dimensions()._height * 0.48, left: dimensions()._width * 0.28}}>
+                <Image source={frogDirectories[defaultFrogIndex + 7].image}/>
+            </View>
+            {/* Brown */}
+            <View style={{position: 'absolute', top: dimensions()._height * 0.49, left: dimensions()._width * 0.45}}>
+                <Image source={frogDirectories[defaultFrogIndex + 8].image}/>
             </View>
 
             {/* Navbar */}
@@ -63,4 +114,22 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%'
     },
+    logoutButton: {
+        position: 'absolute',
+        right: 0,
+        borderRadius: 10,
+        margin: 7,
+        padding: 3,
+        backgroundColor: '#516D67'
+    },
+    messageBox: {
+        position: 'absolute',
+        left: 0,
+        borderRadius: 10,
+        backgroundColor: '#FFE7A1',
+        width: dimensions()._width - 50,
+        height: dimensions()._height * 0.12,
+        margin: 10,
+        padding: 5,
+    }
  });
