13e3d5b32d4cf3a1c69c48926044012aa3653be7
Jayden-Kim-NUS <e1121727@u.nus.edu>
2024-07-26 22:24:14 +0800

    updated leaderboard and homescreen to use async fetch functions

--- screens/LeaderboardScreen.tsx
+++ screens/LeaderboardScreen.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import {useRef, useState, useCallback, useEffect} from 'react';
-import { Button, View, Text, StyleSheet, ImageBackground, FlatList, ScrollView, SafeAreaView, StatusBar, TouchableOpacity, Image, TextInput } from 'react-native';
-import firestore from '@react-native-firebase/firestore';
+import { Button, View, Text, StyleSheet, ImageBackground, FlatList, ScrollView, StatusBar, TouchableOpacity, Image, TextInput } from 'react-native';
 import { frogDirectories, dimensions, showAlert, showAlertAction, parseFUID, getLB, getRankColor} from './../screens/Scripts.tsx';
 
 const SeparatorHorizontalSmall = () => <View style={{marginHorizontal: '1%'}}/>;
@@ -9,39 +8,33 @@ const SeparatorHorizontalSmall = () => <View style={{marginHorizontal: '1%'}}/>;
 function LeaderboardScreen({route, navigation}: {route: any, navigation: any}) {
 
     //LB State
-    const [LB, setLB] = useState<any[]>()
-    async function iniLB() {
-        const retLB = await getLB();
-        setLB(retLB)
-    }
-    iniLB()
+    const [LB, setLB] = useState<any[]>([])
 
     const loadLB = useCallback(async () => {
-          // Replace this with your actual data fetching logic
           const newLB = await getLB();
           setLB(newLB);
-      }, []);
+    }, []);
 
-    // Load achievements when the component mounts
+    // Load when the component mounts
     useEffect(() => {
         loadLB();
         }, [loadLB]);
-    
+
     //User container
-    const UserItem = ({ user } : { user: { rank: number; uid: string; name: string; pfp: number; hours: number } }) => {
+    const UserItem = ({ user } : { user: { rank: number; uid: string; name: string; pfp: number; hours: number }}) => {
         return(
             <View style={styles.friendContainer}>
         <View>
-            <View style={{flexDirection: 'row'}}>
-                <Image source={frogDirectories[user.pfp].image} style={{width: dimensions()._height * 0.05, height: dimensions()._height * 0.0375}}/>
-                <Text style={[styles.friendNameText, {color: getRankColor(user.rank)}]}>{user.name}</Text>
-            </View>   
+            <View style={{flexDirection: 'row', alignItems: 'center',}}>
+                <Image source={frogDirectories[user.pfp].image} style={{width: dimensions()._height * 0.05, height: dimensions()._height * 0.0375,}}/>
+                <Text style={[styles.friendNameText, {color: getRankColor(user.rank), fontSize: user.name.length >= 10 ? 16 : 20,}]}>{user.name}</Text>
+            </View>
         </View>
         
         <View style={{position: 'absolute', right: 20, top: dimensions()._height * 0.01, flexDirection: 'row', alignItems: 'center'}}>
             <Image source={require('./../assets/clock.png')} resizeMode='contain' style={{width: 30, height: 30}}/>
             <SeparatorHorizontalSmall/>
-            <Text style={{fontSize: 20, marginVertical: 10, color: 'white', fontWeight: '300'}}>{Math.floor(user.hours/60)}</Text>
+            <Text style={{fontSize: 20, marginVertical: 10, color: 'white', fontWeight: '300'}}>{user.hours >= 60000 ? Math.floor(user.hours/60000) + 'k' : Math.floor(user.hours/60)}</Text>
         </View>
     </View>
     )}
@@ -63,72 +56,56 @@ function LeaderboardScreen({route, navigation}: {route: any, navigation: any}) {
         {/* Medals */}
         </View>
             <Image source={require('./../assets/first_medal.png')} 
-            style={{width: dimensions()._height * 0.06, height: dimensions()._height * 0.06, position: 'absolute', top: dimensions()._height * 0.192}}/>
+            style={{width: dimensions()._height * 0.06, height: dimensions()._height * 0.06, position: 'absolute', top: dimensions()._height * 0.20}}/>
             <Image source={require('./../assets/second_medal.png')} 
-            style={{width: dimensions()._height * 0.06, height: dimensions()._height * 0.06, position: 'absolute', top: dimensions()._height * 0.292}}/>
+            style={{width: dimensions()._height * 0.06, height: dimensions()._height * 0.06, position: 'absolute', top: dimensions()._height * 0.275 + 20}}/>
             <Image source={require('./../assets/third_medal.png')} 
-            style={{width: dimensions()._height * 0.06, height: dimensions()._height * 0.06, position: 'absolute', top: dimensions()._height * 0.392}}/>
+            style={{width: dimensions()._height * 0.06, height: dimensions()._height * 0.06, position: 'absolute', top: dimensions()._height * 0.35 + 35}}/>
         <View>
-
         </View>
 
-            {/* Leaderboard List */}
-            <View style={[styles.scrollViewContainer, {top: dimensions()._height * 0.12, height: dimensions()._height * 0.76}]}>
-            <FlatList
-            data={LB}
-            renderItem={({ item }) => <UserItem user={item} />}
-            keyExtractor={(item) => item.rank}
-            contentContainerStyle={{ flexGrow: 1}}
-            extraData={LB}
-            />
-            </View>
-        
-{/* 
-            {/* User Object 
-            <View style={styles.friendContainer}>
-        <View>
-            <View style={{flexDirection: 'row'}}>
-                <Image source={pfpDirectory} style={{width: dimensions()._height * 0.05, height: dimensions()._height * 0.0375}}/>
-                <Text style={styles.friendNameText}>John Smith</Text>
-            </View>   
-
-            <Text style={styles.userIdText}>0009-1232</Text>
+        {/* Leaderboard List */}
+        <View style={[styles.scrollViewContainer, {top: dimensions()._height * 0.15, height: dimensions()._height * 0.70, alignSelf: 'flex-end', paddingRight: 20}]}>
+        <FlatList
+        data={LB}
+        renderItem={({ item }) => <UserItem user={item} />}
+        keyExtractor={(item) => item.rank}
+        contentContainerStyle={{ flexGrow: 1}}
+        extraData={LB}
+        />
         </View>
-        
-        <View style={{position: 'absolute', right: 20, top: dimensions()._height * 0.01, flexDirection: 'row', alignItems: 'center'}}>
-            <Image source={require('./../assets/clock.png')} resizeMode='contain' style={{width: 30, height: 30}}/>
-            <SeparatorHorizontalSmall/>
-            <Text style={{fontSize: 20, marginVertical: 10, color: 'white', fontWeight: '300'}}>
-                300h
-            </Text>
+        {/* Navbar */}
+        <View style={{
+            position: 'absolute', 
+            bottom: 0, 
+            justifyContent: 'center', 
+            alignItems: 'center', 
+            backgroundColor: '#516D67', 
+            width: dimensions()._width, 
+            height: dimensions()._height * 0.06, 
+            flexDirection: 'row'
+        }}>
+            <TouchableOpacity style={{flex: 1, width: 40, height: 40, alignItems: 'center'}} 
+                onPress={() => navigation.replace('FriendsList')}>
+                <Image source={require('./../assets/friends_list_alex.png')} style={{height: '100%', width: '100%'}} resizeMode='contain'/>
+            </TouchableOpacity>
+            <TouchableOpacity style={{flex: 1, width: 40, height: 40, alignItems: 'center'}} 
+                onPress={() => navigation.replace('Lock')}>
+                <Image source={require('./../assets/lock.png')} style={{height: '100%', width: '100%'}} resizeMode='contain'/>
+            </TouchableOpacity>
+            <TouchableOpacity style={{flex: 1, width: 40, height: 40, alignItems: 'center'}} 
+                onPress={() => navigation.replace('FrogPond')}>
+                <Image source={require('./../assets/lily_pad2.png')} style={{height: '100%', width: '100%'}} resizeMode='contain'/>
+            </TouchableOpacity>
+            <TouchableOpacity style={{flex: 1, width: 40, height: 40, alignItems: 'center'}} 
+                onPress={() => ''}>
+                <Image source={require('./../assets/trophy.png')} style={{height: '100%', width: '100%'}} resizeMode='contain'/>
+            </TouchableOpacity>
+            <TouchableOpacity style={{flex: 1, width: 40, height: 40, alignItems: 'center'}} 
+                onPress={() => navigation.replace('Profile')}>
+                <Image source={require('./../assets/profile.png')} style={{height: '100%', width: '100%'}} resizeMode='contain'/>
+            </TouchableOpacity>
         </View>
-    </View>
-     */}
-
-            {/* Navbar */}
-            <View style={{position: 'absolute', top: dimensions()._height * 0.94, justifyContent: 'center', alignItems: 'center', backgroundColor: '#516D67', width: dimensions()._width, height: dimensions()._height * 0.2, flexDirection: 'row'}}>
-                <TouchableOpacity style={{position: 'absolute', top: 0, left: dimensions()._width * 0.8 + 20, width: 40, height: 40,}} 
-                    onPress={() => navigation.navigate('Profile')}>
-                    <Image source={require('./../assets/profile.png')} style={{height: '100%', width: '100%'}} resizeMode='contain'/>
-                </TouchableOpacity>
-                <TouchableOpacity style={{position: 'absolute', top: 0, left: dimensions()._width * 0.6 + 20, width: 40, height: 40,}} 
-                    onPress={() => navigation.navigate('Leaderboard')}>
-                    <Image source={require('./../assets/trophy.png')} style={{height: '100%', width: '100%'}} resizeMode='contain'/>
-                </TouchableOpacity>
-                <TouchableOpacity style={{position: 'absolute', top: dimensions()._height * 0.002, left: dimensions()._width * 0.5 - 20, width: 40, height: 40,}} 
-                    onPress={() => navigation.navigate('FrogPond')}>
-                    <Image source={require('./../assets/lily_pad2.png')} style={{height: '100%', width: '100%'}} resizeMode='contain'/>
-                </TouchableOpacity>
-                <TouchableOpacity style={{position: 'absolute', top: 0, right: dimensions()._width * 0.6 + 20, width: 40, height: 40,}} 
-                    onPress={() => navigation.navigate('Lock')}>
-                    <Image source={require('./../assets/lock.png')} style={{height: '100%', width: '100%'}} resizeMode='contain'/>
-                </TouchableOpacity>
-                
-                <TouchableOpacity style={{position: 'absolute', top: 0, right: dimensions()._width * 0.8 + 20, width: 40, height: 40,}} 
-                    onPress={() => navigation.navigate('FriendsList')}>
-                    <Image source={require('./../assets/friends_list_alex.png')} style={{height: '100%', width: '100%'}} resizeMode='contain'/>
-                </TouchableOpacity>
-            </View> 
     </View>
   );
 }
@@ -151,32 +128,25 @@ const styles = StyleSheet.create({
         color: '#FFE7A1',
     },
     friendContainer: {
-        width: dimensions()._width * 0.72,
+        width: dimensions()._width * 0.78,
         height: dimensions()._height * 0.075,
         backgroundColor: '#9AC99B',
         padding: 10,
         margin: 10,
         borderRadius: 8.5,
-        flexDirection: 'row'
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    friendNameText: {
+        fontWeight: 'bold',
+        color: 'white',
+        marginLeft: 10,
     },
     scrollViewContainer: {
         position: 'absolute',
         top: dimensions()._height * 0.15,
         paddingTop: StatusBar.currentHeight,
         alignSelf: 'center',
-    },
-    friendNameText: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: 'white',
-        textAlign: 'left',
-        marginHorizontal: 5,
-    },
-    userIdText: {
-        fontSize: 14,
-        fontWeight: '400',
-        color: '#FFE7A1',
-        marginLeft: 5
     },
     cross: {
         width: dimensions()._height * 0.03,
